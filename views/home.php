41eaacf0df57b1b6736a8654f11fa18167343ca3
hareesh <hareesh.puliyedath@gmail.com>
2015-06-24 19:40:17 -0700

    changes styling from plain css to bootstrap

--- views/home.php
+++ views/home.php
@@ -1,93 +1,137 @@
 <!doctype html>
 <?php
-    session_start();
-    require($_SERVER["DOCUMENT_ROOT"] . '/controllers/new-connection.php');
+session_start();
+require($_SERVER["DOCUMENT_ROOT"] . '/controllers/new-connection.php');
 ?>
 <html>
-    <head>
-        <title>Home</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="css/home.css">
-    </head>
-    <body>
-        <form action="../controllers/logOff.php" method="get">
-            <ul id="header">
-                <li><h2>Coding Dojo Wall</h2></li>
-                <li><h4>Welcome <?php echo $_SESSION['user_name']; ?></h4></li>
-                <li><input type="submit" name="LogOff" value="LogOff"></li>
-            </ul>
-        </form>
+	<head>
+		<title>Home</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class-="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#wallNavBar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Wall</a>
+				</div>
 
-        <form class="message" action="../controllers/postMessage.php" method="post">
-            <ul>
-                <li><h5>Post a Message</h5></li>
-                <li><input type="textarea" name="description" ></li>
-                <li><input type="submit" name="Post a message" value="Post a message"></li>
-            </ul>
-        </form>
+			
+			
+			</div>
+			<div classs="collapse navbar-collapse" id="wallNavBar" >
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="../controllers/logOff.php">LogOff</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span>Welcome, <?php echo $_SESSION['user_name']; ?></a></li>
+				</ul>
+			</div>
+		</nav>
 
-<?php
-    function getComments($post_id){
-        //$messages = fetch("select * from comments where comments.post_id = $post_id");
-        $messages = fetch(join(" ",array(
-            "select comments.created_at, users.user_name,comments.description from comments",
-            "LEFT JOIN users", 
-            "ON comments.user_id = users.id where comments.post_id = $post_id"
-            )));
 
-        $temp="";
-        if (!(array_key_exists('description',$messages))){
-            foreach($messages as $key=>$message){
-                $date = new DateTime($message['created_at']);
-                $formateed_dateTime= $date->format('l jS F Y');
-                $temp = $temp . "<li class='comment pcomment' ><h5>{$message['user_name']} wrote on $formateed_dateTime</h5><p>{$message['description']}</p></li>";
-            }
-        }else{
-            //fix for the fact when the library returns only one row
-            $date = new DateTime($message['created_at']);
-            $formateed_dateTime= $date->format('l jS F Y');
-            $temp = $temp . "<li class='comment pcomment' ><h5>{$messages['user_name']} wrote on $formateed_dateTime</h5><p>{$messages['description']}</p></li>";
-            //$temp = $temp . "<li class='comment pcomment' ><p>{$messages['description']}</p></li>";
-        }
 
-        $temp =  $temp . join("",array(
-            "<li>",
-            '<form class="comment" action="../controllers/postComment.php" method="post">',
-                '<input type="hidden" name="hpost_id" value="' . $post_id . '">',
-                '<input type="textarea" name="description" >',
-                '<input type="submit" name="Post a comment" value="Post a comment">',
-            '</form>',
-            "</li>",
-        ));
-        return $temp;
-    }
-?>
-        <ul>
-<?php 
-    if (array_key_exists('description',$_SESSION['posts'])){
-        $post = $_SESSION['posts'];
-        $date = new DateTime($post['created_at']);
-        $formateed_dateTime= $date->format('l jS F Y');
-        echo "<li class='pmessage message'>";
-        echo "<h5>{$post['user_name']} wrote on $formateed_dateTime</h5>";
-        echo "<p>{$post['description']}</p>";
-        echo "</li>";
-        echo getComments($post['id']);
-    }
-    else{
-        foreach($_SESSION['posts'] as $key=>$post){
-            $date = new DateTime($post['created_at']);
-            $formateed_dateTime= $date->format('l jS F Y');
-            echo "<li class='pmessage message'>";
-            echo "<h5>{$post['user_name']} wrote on $formateed_dateTime</h5>";
-            echo "<p>{$post['description']}</p>";
-            echo "</li>";
-            echo getComments($post['id']);
-        }
-    }
-?>
-        </ul>
+		
+		<div class="container">
+			<form action="../controllers/postMessage.php" method="post">
+				<div class="form-group">
+					<label for="message">Post a Message</label>
+					<input class="form-control" id="message" type="textarea" name="description" >
 
-    </body>
+				</div>
+				<div class="form-group text-right">
+					<input type="submit" class="btn btn-primary" name="Post a message" value="Post a message">
+				</div>
+			</form>
+		</div>
+
+		<?php
+		function getComments($post_id){
+			//$messages = fetch("select * from comments where comments.post_id = $post_id");
+			$messages = fetch(join(" ",array(
+				"select comments.created_at, users.user_name,comments.description from comments",
+					"LEFT JOIN users", 
+					"ON comments.user_id = users.id where comments.post_id = $post_id"
+			)));
+
+			$temp="";
+			if (!(array_key_exists('description',$messages))){
+				foreach($messages as $key=>$message){
+					$date = new DateTime($message['created_at']);
+					$formateed_dateTime= $date->format('l jS F Y');
+					$temp = $temp . "<div class='row'>";
+					$temp = $temp . "<div class='col-md-4'>";
+					$temp = $temp . "</div>";
+					$temp = $temp . "<div class='col-md-8'>";
+					$temp = $temp . "<div class='panel panel-success'><div class='panel-heading'><h5>{$message['user_name']} wrote on $formateed_dateTime</h5></div><p>{$message['description']}</p></div>";
+					$temp = $temp . "</div>";
+					$temp = $temp . "</div>";
+				}
+			}else{
+				//fix for the fact when the library returns only one row
+				$date = new DateTime($message['created_at']);
+				$formateed_dateTime= $date->format('l jS F Y');
+				$temp = $temp . "<div class='panel panel-info' ><h5>{$messages['user_name']} wrote on $formateed_dateTime</h5><p>{$messages['description']}</p></div>";
+				//$temp = $temp . "<li class='comment pcomment' ><p>{$messages['description']}</p></li>";
+			}
+
+			$temp =  $temp . join("",array(
+				'<div class="row">',
+					'<div class="col-md-4">',
+					'</div>',
+					'<form class="col-md-8" action="../controllers/postComment.php" method="post">',
+					"<div class='form-group'>",
+					'<input type="hidden" name="hpost_id" value="' . $post_id . '">',
+					'<input class="form-control" type="textarea" name="description" >',
+					"</div>",
+					"<div class='form-group text-right'>",
+					'<input class="btn btn-primary" type="submit" name="Post a comment" value="Post a comment">',
+					'</div>',
+					'</form>',
+					'</div>'
+			));
+			return $temp;
+		}
+		?>
+		<div class="container">
+			<?php 
+			if (array_key_exists('description',$_SESSION['posts'])){
+				$post = $_SESSION['posts'];
+				$date = new DateTime($post['created_at']);
+				$formateed_dateTime= $date->format('l jS F Y');
+				echo "<div class='panel panel-info'>";
+				echo "<div class='panel-heading'>";
+				echo "<h5>{$post['user_name']} wrote on $formateed_dateTime</h5>";
+				echo "</div>";
+				echo "<div class='panel-content'>";
+				echo "<p>{$post['description']}</p>";
+				echo "</div>";
+				echo "</div>";
+				echo getComments($post['id']);
+			}
+			else{
+				foreach($_SESSION['posts'] as $key=>$post){
+					$date = new DateTime($post['created_at']);
+					$formateed_dateTime= $date->format('l jS F Y');
+					echo "<div class='panel panel-info'>";
+					echo "<div class='panel-heading'>";
+					echo "<h5>{$post['user_name']} wrote on $formateed_dateTime</h5>";
+					echo "</div>";
+					echo "<div class='panel-content'>";
+					echo "<p>{$post['description']}</p>";
+					echo "</div>";
+					echo "</div>";
+					echo getComments($post['id']);
+				}
+			}
+			?>
+		</div>
+	</body>
 
 </html>
--- views/index.php
+++ views/index.php
@@ -3,36 +3,78 @@
 session_start();
 ?>
 <html>
-    <head>
-        <title></title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="css/index.css">
-    </head>
-    <body>
-        <div id="container">
-            <h1>The Wall</h1>
-            <form id="login" action="../controllers/register.php" method="post">
-                <ul>
-                    <li><input type="text" name="user_name" placeholder="username"></li>
-                    <li><input type="text" name="password" placeholder="password"></li>
-                    <li><input type="submit" name="action" value="login"><p> <?php echo $_SESSION['login'] ?></li>
-                </ul>
-            </form>
-            <form id="register" action="../controllers/register.php" method="post">
-                <ul>
-                    <li><input type="text" name="first_name" placeholder="FirstName"><p><?php echo $_SESSION['errors']['first_name']?></p></li>
-                    <li><input type="text" name="last_name" placeholder="LastName"><p><?php echo $_SESSION['errors']['last_name']?></p></li>
-                    <li><input type="text" name="user_name" placeholder="UserName"><p><?php echo $_SESSION['errors']['user_name']?></p></li>
-                    <li><input type="text" name="email" placeholder="Email"><p><?php echo $_SESSION['errors']['email']?></p></li>
-                    <li><input type="text" name="password" placeholder="Password"><p><?php echo $_SESSION['errors']['password']?></p></li>
-                    <li><input type="submit" name="action" value="register"  ><p><?php echo $_SESSION['register'] ?></p></li>
-                </ul>
-            </form>
-        </div>
-        <?php
-           $_SESSION['errors'] = null;
-           $_SESSION['login'] = null;
-           $_SESSION['register'] = null;
-           ?>
-    </body>
+	<head>
+		<meta charset="utf-8">
+		<title>Coding Dojo - The Wall</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	</head>
+	<body>
+
+		<div class="container">
+			<div class="jumbotron">
+				<h1>Coding Dojo, The Wall</h1>
+				<p>Start Posting</p>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<form class="col-xs-12 col-sm-12 col-md-6" id="login" action="../controllers/register.php" method="post">
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+							<input id="userName" class="form-control" type="text" name="user_name" placeholder="username">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+							<input class="form-control" id="userPassword" type="text" name="password" placeholder="password">
+						</div>
+						
+					</div>
+					<input class="btn btn-primary" type="submit" name="action" value="login"><p> <?php echo $_SESSION['login'] ?>
+				</form>
+				<form class="col-xs-12 col-sm-12 col-md-6 col-md-6" id="register" action="../controllers/register.php" method="post" >
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+							<input type="text" class="form-control" name="first_name" placeholder="FirstName"><p><?php echo $_SESSION['errors']['first_name']?></p>
+						</div>
+
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+							<input type="text" name="last_name" class="form-control" placeholder="LastName"><p><?php echo $_SESSION['errors']['last_name']?></p>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+							<input type="text" name="user_name" class="form-control" placeholder="UserName"><p><?php echo $_SESSION['errors']['user_name']?></p>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+							<input type="text" name="email" class="form-control" placeholder="Email"><p><?php echo $_SESSION['errors']['email']?></p>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+							<input type="text" name="password" class="form-control" placeholder="Password"><p><?php echo $_SESSION['errors']['password']?></p>
+						</div>
+					</div>
+					<input class="btn btn-primary" type="submit" name="action" value="register"  ><p><?php echo $_SESSION['register'] ?></p>
+				</form>
+			</div>
+		</div>
+		<?php
+		$_SESSION['errors'] = null;
+		$_SESSION['login'] = null;
+		$_SESSION['register'] = null;
+		?>
+	</body>
 </html>
